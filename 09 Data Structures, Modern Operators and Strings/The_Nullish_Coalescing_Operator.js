b3fa8f322cda5b706acc252146ea4a7f06775145
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-26 23:18:23 +0530

    Nullish Coalescing Operator added

---
+++ 09 Data Structures, Modern Operators and Strings/The_Nullish_Coalescing_Operator.js
@@ -0,0 +1,5 @@
+/*
+it is an operator that was introduced in ES2020, named The Nullish Coalescing Operator.
+- it works on the nullish value that are null and undefined values. it also include zero and empty string. and if the first value is nullish then the second statement will be executed.
+*/
+
