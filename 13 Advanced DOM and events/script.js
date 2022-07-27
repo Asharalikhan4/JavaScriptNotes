cb3945966a53602e074a5a93777c8f33c3c51ae5
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-07-27 19:17:58 +0530

    Select add and delete completed

--- 13 Advanced DOM and events/script.js
+++ 13 Advanced DOM and events/script.js
@@ -29,3 +29,49 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+// Selecting, Creating and Deleting element
+
+
+console.log(document.documentElement);
+console.log(document.head);
+
+const header = document.querySelector('.header');  // to select only one element.
+const allSections = document.querySelectorAll('.section');   // to select multiple element.
+console.log(allSections);
+
+
+document.getElementById('section--1');   // get all the elements with this id.
+const allButtons = document.getElementsByTagName('button');  // get all the elements with this name.
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));  // gets all the elements with this class name.
+
+// All these are selector but the one we are going to use the most is the query selector and query selector all.
+
+
+
+// CREATING AND INSERTING ELEMENTS
+// insertAdjacentHTML
+
+const message = document.createElement('div');  // it will create a dom element and then store some msg init.
+message.classList.add('cookie-message');
+message.innerHTML = 'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';  // this will add the text content in this div.
+// Now we can select the element in which we want to insert.let add this is header
+//header.prepend(message);   // this is added as the first child we can also add it as the last child using append.
+
+header.append(message);
+
+// to make it availble on more then one place we first make to copy it.
+
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// DELETING AND ELEMENT
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+  message.remove();
+  // old syntax - message.parentElement.removeChild(message);
+});``
